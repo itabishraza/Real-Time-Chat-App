0db4daf21b4b76bfe36fb7e2fb396fbc01f9d1ac
Amrit Rai <iamamrit27@gmail.com>
2024-07-30 21:00:32 +0530

    add login w providers

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Button from "./components/ui/Button";
+
 export default async function Home() {
-  return <div className="text-red-500">hi</div>
+  return <Button variant="ghost">Hellow</Button>
 }
