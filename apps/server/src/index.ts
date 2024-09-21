f85d66357b3b530319cd33c9a254a78321f3004b
Amrit Rai <iamamrit27@gmail.com>
2024-09-21 10:27:24 +0530

    turbo init

---
+++ apps/server/src/index.ts
@@ -0,0 +1,12 @@
+import http from "http";
+
+async function init() {
+    const httpServer = http.createServer();
+    const PORT = process.env.PORT || 8000;
+
+    httpServer.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+init();
