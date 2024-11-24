aff97ed3dab888fd6e9931703843a424a8bb917a
Amrit <iamamrit27@gmail.com>
2024-11-24 21:19:19 +0530

    make ts happy

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -20,7 +20,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000" || "https://real-time-chat-amritwt.vercel.app" || "https://real-time-chat-git-main-amritwt.vercel.app/"],
+    origin: ["http://localhost:3000", "https://real-time-chat-amritwt.vercel.app", "https://real-time-chat-git-main-amritwt.vercel.app/"],
     methods: ["GET", "POST"]
   }
 });
